1cce621072ee1c2c4c7a6f62550f87d745ea8ac4
gefarion <gefarion@d4d9b3dc-7bab-1ab7-89c5-c9d964c8364c>
2013-12-02 17:15:31 +0000

    Operaciones entre buffers: add, sub, mul, div, concat

--- test.py
+++ test.py
@@ -14,9 +14,16 @@ sound = generator.note('D', 1)
 sound = sound.loop(3)
 # sound.plot()
 
-sound = generator.array([-1, 1])
-sound.post()
-sound = sound.loop(1003).expand().loop(12).play();
+base = generator.array([-1, 1])
+sound = generator.array([]);
+
+for i in xrange(10,20):
+	sound //= base.loop(i).expand().loop(3);
+for i in xrange(20,10, -1):
+	sound //= base.loop(i).expand().loop(3);
+
+sound.play()
+
 
 
 
