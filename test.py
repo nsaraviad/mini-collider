ea947f0e0a33e42431fee4815a5d4600196a85c5
gefarion <gefarion@d4d9b3dc-7bab-1ab7-89c5-c9d964c8364c>
2013-12-03 03:24:53 +0000

    operador mix y generador noise

--- test.py
+++ test.py
@@ -6,20 +6,21 @@ beat = sample_rate / 12
 minicollider.init(sample_rate, beat)
 
 generator = minicollider.SoundGenerator()
-# sound = generator.sine(23, 1)
-sound = generator.note('D', 1)
-# sound.plot()
-sound = sound.loop(3)
-# sound.plot()
+
+sound1 = generator.array([0, 0.1, 0.2])
+sound2 = generator.array([0.2, 0.4, 0.5])
+
+sound3 = sound1 // sound2
+sound3.post()
+
+# print generator.noise(0.1).loop(24).play()
 
 base = generator.array([-1, 1])
 sound = generator.array([]);
-
 for i in xrange(10,20):
 	sound //= base.loop(i).expand().loop(3);
 for i in xrange(20,10, -1):
 	sound //= base.loop(i).expand().loop(3);
-
 sound.play()
 
 
