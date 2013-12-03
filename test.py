cfb3cb4f70dc6683d9adb06bdfd1113ecddb787d
gefarion <gefarion@d4d9b3dc-7bab-1ab7-89c5-c9d964c8364c>
2013-12-03 22:41:26 +0000

    fixes en el lexer

--- test.py
+++ test.py
@@ -13,15 +13,18 @@ sound2 = generator.array([0.2, 0.4, 0.5])
 sound3 = sound1 // sound2
 sound3.post()
 
+sound4 = generator.sine(20, 1);
+sound4.plot()
+
 # print generator.noise(0.1).loop(24).play()
 
-base = generator.array([-1, 1])
-sound = generator.array([]);
-for i in xrange(10,20):
-	sound //= base.loop(i).expand().loop(3);
-for i in xrange(20,10, -1):
-	sound //= base.loop(i).expand().loop(3);
-sound.play()
+# base = generator.array([-1, 1])
+# sound = generator.array([]);
+# for i in xrange(10,20):
+# 	sound //= base.loop(i).expand().loop(3);
+# for i in xrange(20,10, -1):
+# 	sound //= base.loop(i).expand().loop(3);
+# sound.play()
 
 
 
