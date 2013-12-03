20f62789d9a806b2f3a66a00f978c946fcce7e24
santiher14@gmail.com <santiher14@gmail.com@d4d9b3dc-7bab-1ab7-89c5-c9d964c8364c>
2013-12-03 13:06:11 +0000

    Lexer

---
+++ lexer.py
@@ -0,0 +1,55 @@
+
+# -----------------------------------------------------------------------------
+# lexer.py
+#
+# Lexer para el mini-collider
+# -----------------------------------------------------------------------------
+
+tokens = (
+    'NUM', 'SIN', 'LIN', 'SIL', 'NOI', 'WS', 'PLAY', 'POST', 'LOOP',
+    'TUNE', 'FILL', 'REDU', 'EXPA', 'CON', 'MIX', 'ADD', 'SUB', 'MUL', 
+    'DIV', 'LPAREN', 'RPAREN', 'LLLAVE', 'RLLAVE', 'COMM',
+    )
+
+# Tokens
+
+t_SIN  = r'sin'
+t_LIN  = r'lin|linear'
+t_SIL  = r'sil|silence'
+t_NOI  = r'noi|noise'
+t_PLAY = r'play'
+t_POST = r'post'
+t_LOOP = r'loop'
+t_TUNE = r'tune'
+t_FILL = r'fill'
+t_REDU = r'reduce'
+t_EXPA = r'expand'
+t_CON  = r'con|;'
+t_MIX  = r'mix|&'
+t_ADD  = r'add|\+'
+t_SUB  = r'sub|-'
+t_MUL  = r'mul|\*'
+t_DIV  = r'div|/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_LLLAVE = r'\{'
+t_RLLAVE = r'\}'
+t_ignore_WS   = r'\s|\t|\n'
+t_ignore_COMM = r'//.*$'
+
+def t_NUM(t):
+    r'(\+|-)?[0-9]+(\.[0-9]+)?'
+    try:
+        t.value = float(t.value)
+    except ValueError:
+        print("Number value too large %d", t.value)
+        t.value = 0
+    return t
+    
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+    
+# Build the lexer
+import ply.lex as lex
+lex.lex()
