29f841dcb9feb1ea63384c859833d312bf05cd90
gefarion <gefarion@d4d9b3dc-7bab-1ab7-89c5-c9d964c8364c>
2013-12-05 04:02:56 +0000

    Base para los tests

--- minicollider/test/test_mixer.py
+++ minicollider/test/test_mixer.py
@@ -1,4 +1,19 @@
 import minicollider.mixer as mixer
+import unittest
+
+class TestSoundGeneratorCases(unittest.TestCase):
+
+	def setUp(self):
+		self.sample_rate = 48
+		self.beat = self.sample_rate / 12
+
+		mixer.init(self.sample_rate, self.beat, 0)
+		self.generator = mixer.SoundGenerator()
+
+	def test_from_list(self):
+		pass
 
 
+if __name__ == '__main__':
+	unittest.main()
 
--- minicollider/test/test_parser.py
+++ minicollider/test/test_parser.py
@@ -1,1 +1,19 @@
 import minicollider.parser as parser
+import unittest
+
+class TestGeneratorsCases(unittest.TestCase):
+
+	def setUp(self):
+		self.sample_rate = 48
+		self.beat = self.sample_rate / 12
+
+		parser.init(self.sample_rate, self.beat, 0)
+		
+
+	def test_from_list(self):
+		pass
+
+
+if __name__ == '__main__':
+	unittest.main()
+
