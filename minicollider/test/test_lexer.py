e3910fce55e4d42949862ef7b04c7dbfe3154c12
gefarion <gefarion@d4d9b3dc-7bab-1ab7-89c5-c9d964c8364c>
2013-12-05 03:09:54 +0000

    reestructuracion de los modulos

--- minicollider/test/test_lexer.py
+++ minicollider/test/test_lexer.py
@@ -1,17 +1,1 @@
-from lexer import lexer 
-
-# Test it out
-data = '''
-{ { 1 - 2} con sin(-3) }.play
-'''
-
-# Give the lexer some input
-lexer.input(data)
-
-# Tokenize
-while True:
-    tok = lexer.token()
-    if not tok: break      # No more input
-    print tok
-
-
+import minicollider.lexer as lexer
--- minicollider/test/test_parser.py
+++ minicollider/test/test_parser.py
@@ -1,8 +1,1 @@
-while 1:
-		try:
-				s = input('buff > ')   # Use raw_input on Python 2
-		except EOFError:
-				break
-		if s == 'quit':
-				break
-		yacc.parse(s)
+import minicollider.parser as parser
